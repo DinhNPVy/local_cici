498748d52c92491cb8623d1c39d75abaa019505a
DinhNPVy <4501104282@student.hcmue.edu.vn>
2021-11-07 23:44:44 +0700

    upload pagination

--- application/controllers/admin/Product.php
+++ application/controllers/admin/Product.php
@@ -19,24 +19,55 @@ class Product extends MY_Controller
         //load thu vien phan trang
         $this->load->library('pagination');
         $config = array();
-        $config['total_rows'] = ''; // tong tat ca san pham tren website
+        $config['total_rows'] = $total_rows; // tong tat ca san pham tren website
         $config['base_url'] = admin_url('product/index'); // link hiển thị ra danh sách sản phẩm
         $config['per_page'] = 3; // số lượng hiển thị trên 1 trang
         $config['uri_segment'] = 4; // lấy ra phân đoạn hiện trên link url
-        $config['next_link'] = 'Next page';
-        $config['pre_link'] = 'Previous page';
+        $config['next_link'] = 'Next';
+        $config['prev_link'] = 'Previous';
 
         $this->pagination->initialize($config);
 
         // lay dữ liệu phân trang
         $segment = $this->uri->segment(4);
         $segment = intval($segment);
+
         $input = array();
         $input['limit'] = array($config['per_page'], $segment);
 
+        //kiem ra co thuc hien lọc kh 
+        $id = $this->input->get('id');
+        $id = intval($id);
+        $input['where'] = array();
+        if ($id > 0) {
+            $input['where']['id'] = $id;
+        }
+        $name = $this->input->get('name');
+        if ($name) {
+            $input['like'] = array('name', $name);
+        }
+        $catalog_id = $this->input->get('catalog');
+        $catalog_id = intval($catalog_id);
+        if ($catalog_id > 0) {
+            $input['where']['catalog_id'] = $catalog_id;
+        }
         // lay ra đanh sách sản phẩm
         $list = $this->product_model->get_list($input);
         $this->data['list'] = $list;
+
+
+        // lay danh sach danh muc san pham
+        $this->load->model('catalog_model');
+        $input = array();
+        $input['where'] = array('parent_id' => 0);
+        $catalogs = $this->catalog_model->get_list($input);
+        foreach ($catalogs as $row) {
+            $input['where'] = array('parent_id' => $row->id);
+            $subs = $this->catalog_model->get_list($input);
+            $row->subs = $subs;
+        }
+        $this->data['catalogs'] = $catalogs;
+
 
         //lay noi dung cua bien message
         $message = $this->session->flashdata('message');
--- application/views/admin/product/index.php
+++ application/views/admin/product/index.php
@@ -6,7 +6,74 @@
         <div class="card-header pb-0 p-3">
             <h6 class="mb-1">Product</h6>
             <p class="text-sm">There are currently <?php echo $total_rows ?> products</p>
+            <div class="pagination-sm pagination-primary page-item" style="float: right;">
+
+                <?php echo $this->pagination->create_links() ?>
+
+            </div>
+
+            <div>
+                <form method="get" action="<?php echo admin_url('product') ?>">
+                    <table class="navbar-nav  justify-content-end" style="display: -webkit-inline-box;">
+
+                        <tr class="nav-item d-flex align-items-center">
+                            <td href="#" class="nav-link text-body font-weight-bold px-0">
+                                ID
+                                <input class="form-control" id="filter_id" value="<?php echo $this->input->get('id') ?>" type="text" name="id" />
+                                &nbsp;
+                            </td>
+
+
+
+                            <td href="#" class="nav-link text-body font-weight-bold px-0">
+
+                                Name
+                                <input class="form-control" type="text" value="<?php echo $this->input->get('name') ?>" id="filter_name" name="name" />
+                                &nbsp;
+                            </td>
+
+
+
+                            <td href="#" class="nav-link text-body font-weight-bold px-0">
+                                Type
+                                <select name="catalog" class="form-control">
+                                    <option value="">Choose type</option>
+                                    <!-- kiem tra xem co danh muc con hay khong -->
+                                    <?php foreach ($catalogs as $row) : ?>
+                                        <?php if (count($row->subs) > 1) : ?>
+                                            <optgroup label="<?php echo $row->name ?>">
+                                                <?php foreach ($row->subs as $sub) : ?>
+                                                    <option value="<?php echo $sub->id ?>" <?php echo ($this->input->get('catalog') == $sub->id) ? 'selected' : '' ?>>
+                                                        <?php echo $sub->name ?>
+                                                    </option>
+                                                <?php endforeach ?>
+                                            </optgroup>
+                                        <?php else : ?>
+                                            <option value="<?php echo $row->id ?>" <?php echo ($this->input->get('catalog') == $row->id) ? 'selected' : '' ?>>
+                                                <?php echo $row->name ?>
+                                            </option>
+                                        <?php endif ?>
+                                    <?php endforeach ?>
+
+                                </select>
+                                &nbsp;
+                            </td>
+
+                        </tr>
+                        <td class="d-flex align-items-center justify-content">
+                            <input type="submit" class="btn btn-outline-primary btn-sm mb-0" value="Search">
+                            &nbsp;
+                            <input type="reset" onclick="window.location.href = '<?php echo admin_url('product')  ?>'" class="btn btn-outline-primary btn-sm mb-0" value="Reset">
+                        </td>
+                    </table>
+
+                </form>
+            </div>
+
+
         </div>
+
+
         <div class="card-body p-3">
             <div class="row">
                 <?php foreach ($list as $row) : ?>
@@ -23,6 +90,15 @@
                                     <h5>
                                         <?php echo $row->name ?>
                                     </h5>
+                                    <h6>
+                                        <?php if ($row->discount > 0) : ?>
+                                            <?php $price_new = $row->price - $row->discount; ?>
+                                            <b style="color:crimson;"><?php echo number_format($price_new) ?> VNĐ</b>
+                                            <p style="text-decoration:line-through"><?php echo number_format($row->price) ?> VNĐ</p>
+                                        <?php else : ?>
+                                            <b style="color:crimson;"><?php echo number_format($row->price) ?>VNĐ</b>
+                                        <?php endif; ?>
+                                    </h6>
                                 </a>
                                 <p class="mb-4 text-sm">
                                     As Uber works through a huge amount of internal management turmoil.
@@ -30,18 +106,11 @@
                                 <div class="d-flex align-items-center justify-content-between">
                                     <!-- <button type="button" class="btn btn-outline-primary btn-sm mb-0">View Project</button> -->
                                     <div class="avatar-group mt-2">
-                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Peterson">
-                                            <img alt="Image placeholder" src="<?php echo public_url('admin/assets') ?>/img/team-4.jpg">
-                                        </a>
-                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Nick Daniel">
-                                            <img alt="Image placeholder" src="<?php echo public_url('admin/assets') ?>/img/team-3.jpg">
-                                        </a>
-                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Ryan Milly">
-                                            <img alt="Image placeholder" src="<?php echo public_url('admin/assets') ?>/img/team-2.jpg">
-                                        </a>
-                                        <a href="javascript:;" class="avatar avatar-xs rounded-circle" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Elena Morison">
-                                            <img alt="Image placeholder" src="<?php echo public_url('admin/assets') ?>/img/team-1.jpg">
-                                        </a>
+
+                                        <span>Buyed: <?php echo $row->buyed ?></span>
+
+                                        <span>View: <?php echo $row->view ?></span>
+
                                     </div>
                                 </div>
 
